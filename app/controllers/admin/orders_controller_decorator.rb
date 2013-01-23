5899e4ca10e870cfd7cc5ffa9ac28c1a3189c744
GeNiuS69 <alex4578@yandex.ru>
2013-01-23 16:55:24 +0400

    FTP

--- app/controllers/admin/orders_controller_decorator.rb
+++ app/controllers/admin/orders_controller_decorator.rb
@@ -1,16 +1,17 @@
+require 'net/ftp'
 Admin::OrdersController.class_eval do
     def export
         f = File.new('xml/' + @order.number + '.xml','w')
-        f.puts('<?xml version="1.0" encoding="windows-1251" standalone="no"?>')
+        f.puts('<?xml version="1.0" encoding="utf-8" standalone="no"?>')
         f.puts('<DSOrders>')
 
-        f.puts('<ORDER NUMBER="'+@order.number+'" DATE="' + @order.created_at.to_s + '" CLIENT_PHONE="'+ @order.ship_address.phone+'" CLIENT_ADDRESS="'+@order.ship_address.address1 + '"></ORDER>')
+        f.puts('<ORDER NUMBER="'+@order.number+'" DATE="' + @order.created_at.strftime("%Y-%m-%d %H:%M:%S") + '" CLIENT_PHONE="'+ @order.ship_address.phone+'" CLIENT_ADDRESS="'+@order.ship_address.address1 + '"></ORDER>')
         @order.line_items.each do |item|
             f.print('<goods good_1s="')
             if (Variant.find_by_id(item.variant_id).code_1c.nil?)
-                f.print(Product.find_by_id(Variant.find_by_id(item.variant_id).product_id).code_1c.nil? ? "no_kod_1c" : Product.find_by_id(Variant.find_by_id(item.variant_id).product_id).code_1c.to_s )
+                f.print(Product.find_by_id(Variant.find_by_id(item.variant_id).product_id).code_1c.nil? ? "no_kod_1c" : Product.find_by_id(Variant.find_by_id(item.variant_id).product_id).code_1c )
             else
-                f.print(Variant.find_by_id(item.variant_id).code_1c.to_s)
+                f.print(Variant.find_by_id(item.variant_id).code_1c)
             end
             f.print('" good_count="')
             f.print(item.quantity.to_s)
@@ -18,6 +19,12 @@ Admin::OrdersController.class_eval do
         end
         f.puts('</DSOrders>')
         f.close
+
+        ftp = Net::FTP.open()
+        ftp.put(f.path)
+        File.delete(f.path)
+        ftp.close
+
         redirect_to admin_orders_path, :notice => t(:succesful_export)
     end
 end
